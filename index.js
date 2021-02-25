4371255aa7f36518dbe6cdc46717ba4bbf7a5a56
piyatida potisut <60160251@go.buu.ac.th>
2021-02-25 18:54:17 +0700

    feat: POST/movie v2.0

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+let movies = []
+
+app.get('/movies',(req,res) => {
+    //input 
+
+    
+    //process
+
+    
+    //output
+res.status(200).json(movies)
+})
+
+
+// POST /movies
+app.post('/movies',(req,res) =>{
+//input
+
+let newtitle = req.body.title
+let newplot = req.body.plot
+let newGenres = req.body.genres
+let newCreators = req.body.newcreators
+let newStars = req.body.stars
+let newRating = req.body.rating
+//rating
+//- score
+//- vote
+
+//key: value
+let newMovie = {
+    title: title,
+    plot :newplot,
+    genres:newGenres,
+    creators: newCreators,
+    stars: newStars,
+    rating: newRating
+
+}
+let movieID = 0
+
+
+//process
+movies.push(newMovie)
+// n-1
+movieID = movies.length - 1 
+
+//output
+res.status(201).json(movieID)
+})
+const port = 3000
+app.listen(port,() => console.log(`Server started at ${port}`))
